51f92dcd4612a2a6b8236d22d5355adbc16365e9
Luna <hernandezguerraluna@gmail.com>
2023-11-06 23:07:51 +0000

    Weather and Location classes

--- src/main/java/org/example/model/Location.java
+++ src/main/java/org/example/model/Location.java
@@ -1,7 +1,46 @@
 package org.example.model;
 
 public class Location {
-    private double lat;
-    private double lon;
+    private String lat;
+    private String lon;
     private String island;
+
+    public Location(String lat, String lon, String island) {
+        this.lat = lat;
+        this.lon = lon;
+        this.island = island;
+    }
+
+    public String getLat() {
+        return lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    public String getLon() {
+        return lon;
+    }
+
+    public void setLon(String lon) {
+        this.lon = lon;
+    }
+
+    public String getIsland() {
+        return island;
+    }
+
+    public void setIsland(String island) {
+        this.island = island;
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" +
+                "lat='" + lat + '\'' +
+                ", lon='" + lon + '\'' +
+                ", island='" + island + '\'' +
+                '}';
+    }
 }
--- src/main/java/org/example/model/Weather.java
+++ src/main/java/org/example/model/Weather.java
@@ -4,10 +4,101 @@ import java.time.Instant;
 
 public class Weather {
     private double temp;
-    private int pop;
+    private double pop;
     private int humidity;
     private int clouds;
     private double windSpeed;
+    private String dateTime;
     private Instant ts;
     private Location location;
+
+    public Weather(double temp, double pop, int humidity, int clouds, double windSpeed, String dateTime,
+                   Instant ts, Location location) {
+        this.temp = temp;
+        this.pop = pop;
+        this.humidity = humidity;
+        this.clouds = clouds;
+        this.windSpeed = windSpeed;
+        this.dateTime = dateTime;
+        this.ts = ts;
+        this.location = location;
+    }
+
+    public double getTemp() {
+        return temp;
+    }
+
+    public void setTemp(double temp) {
+        this.temp = temp;
+    }
+
+    public double getPop() {
+        return pop;
+    }
+
+    public void setPop(double pop) {
+        this.pop = pop;
+    }
+
+    public int getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(int humidity) {
+        this.humidity = humidity;
+    }
+
+    public int getClouds() {
+        return clouds;
+    }
+
+    public void setClouds(int clouds) {
+        this.clouds = clouds;
+    }
+
+    public double getWindSpeed() {
+        return windSpeed;
+    }
+
+    public void setWindSpeed(double windSpeed) {
+        this.windSpeed = windSpeed;
+    }
+
+    public String getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(String dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public Instant getTs() {
+        return ts;
+    }
+
+    public void setTs(Instant ts) {
+        this.ts = ts;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    @Override
+    public String toString() {
+        return "Weather{" +
+                "temp=" + temp +
+                ", pop=" + pop +
+                ", humidity=" + humidity +
+                ", clouds=" + clouds +
+                ", windSpeed=" + windSpeed +
+                ", dateTime='" + dateTime + '\'' +
+                ", ts=" + ts +
+                ", location=" + location +
+                '}';
+    }
 }
