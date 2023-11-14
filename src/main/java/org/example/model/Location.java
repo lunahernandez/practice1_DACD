c84f273743f5ea830fc11472d353898a5df8f194
lunahernandez <145288658+lunahernandez@users.noreply.github.com>
2023-11-14 16:57:21 +0000

    Weather and Location as records

--- src/main/java/org/example/model/Location.java
+++ src/main/java/org/example/model/Location.java
@@ -1,28 +1,6 @@
 package org.example.model;
 
-public class Location {
-    private final String lat;
-    private final String lon;
-    private final String island;
-
-    public Location(String lat, String lon, String island) {
-        this.lat = lat;
-        this.lon = lon;
-        this.island = island;
-    }
-
-    public String getLat() {
-        return lat;
-    }
-
-    public String getLon() {
-        return lon;
-    }
-
-
-    public String getIsland() {
-        return island;
-    }
+public record Location(String lat, String lon, String island) {
 
 
     @Override
--- src/main/java/org/example/model/Weather.java
+++ src/main/java/org/example/model/Weather.java
@@ -1,64 +1,8 @@
 package org.example.model;
 
 
-public class Weather {
+import java.time.Instant;
 
-    private final String dateTime;
-    private final Location location;
-    private final double temp;
-    private final double pop;
-    private final int humidity;
-    private final int clouds;
-    private final double windSpeed;
-
-    public Weather(String dateTime, Location location, double temp, double pop, int humidity, int clouds, double windSpeed) {
-        this.dateTime = dateTime;
-        this.location = location;
-        this.temp = temp;
-        this.pop = pop;
-        this.humidity = humidity;
-        this.clouds = clouds;
-        this.windSpeed = windSpeed;
-    }
-
-    public String getDateTime() {
-        return dateTime;
-    }
-
-    public Location getLocation() {
-        return location;
-    }
-
-    public double getTemp() {
-        return temp;
-    }
-
-    public double getPop() {
-        return pop;
-    }
-
-    public int getHumidity() {
-        return humidity;
-    }
-
-    public int getClouds() {
-        return clouds;
-    }
-
-    public double getWindSpeed() {
-        return windSpeed;
-    }
-
-    @Override
-    public String toString() {
-        return "Weather{" +
-                "dateTime='" + dateTime + '\'' +
-                ", location=" + location +
-                ", temp=" + temp +
-                ", pop=" + pop +
-                ", humidity=" + humidity +
-                ", clouds=" + clouds +
-                ", windSpeed=" + windSpeed +
-                '}';
-    }
+public record Weather(Instant ts, Location location, double temp, double pop, int humidity, int clouds,
+                      double windSpeed) {
 }
