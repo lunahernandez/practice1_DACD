7b2f3361b0cfce4dd4d0c7581e7982c75a4e670d
Luna <hernandezguerraluna@gmail.com>
2023-11-26 22:45:16 +0000

    new Weather design

--- prediction-provider/src/main/java/hernandez/guerra/model/Weather.java
+++ prediction-provider/src/main/java/hernandez/guerra/model/Weather.java
@@ -3,5 +3,5 @@ package hernandez.guerra.model;
 
 import java.time.Instant;
 
-public record Weather(Instant ts, Location location, double temp, double pop, int humidity, int clouds,
-                      double windSpeed) {}
+public record Weather(Instant ts, String ss, Instant predictionTime, Location location,
+                      double temp, double pop, int humidity, int clouds, double windSpeed) {}
