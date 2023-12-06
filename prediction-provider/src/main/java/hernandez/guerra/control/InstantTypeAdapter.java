099fef76f7b267ee0fc2c2aad867c02c6d956812
lunahernandez <145288658+lunahernandez@users.noreply.github.com>
2023-12-06 19:29:12 +0000

    New InstantTypeAdapter

--- prediction-provider/src/main/java/hernandez/guerra/control/InstantTypeAdapter.java
+++ prediction-provider/src/main/java/hernandez/guerra/control/InstantTypeAdapter.java
@@ -1,20 +1,21 @@
 package hernandez.guerra.control;
 
-import com.google.gson.*;
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
 
-import java.lang.reflect.Type;
+import java.io.IOException;
 import java.time.Instant;
 
-public class InstantTypeAdapter implements JsonSerializer<Instant>, JsonDeserializer<Instant> {
+public class InstantTypeAdapter extends TypeAdapter<Instant> {
 
     @Override
-    public JsonElement serialize(Instant instant, Type type, JsonSerializationContext jsonSerializationContext) {
-        return new JsonPrimitive(instant.toString());
+    public void write(JsonWriter jsonWriter, Instant instant) throws IOException {
+        jsonWriter.value(instant.toString());
     }
 
     @Override
-    public Instant deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext)
-            throws JsonParseException {
-        return Instant.parse(jsonElement.getAsString());
+    public Instant read(JsonReader jsonReader) throws IOException {
+        return Instant.parse(jsonReader.nextString());
     }
 }
