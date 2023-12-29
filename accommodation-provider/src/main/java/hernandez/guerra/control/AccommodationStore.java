8ee639d509728a74467532cc79a138758326234f
Luna <hernandezguerraluna@gmail.com>
2023-12-29 19:36:53 +0000

    AccommodationStore

--- accommodation-provider/src/main/java/hernandez/guerra/control/AccommodationStore.java
+++ accommodation-provider/src/main/java/hernandez/guerra/control/AccommodationStore.java
@@ -1,4 +1,8 @@
 package hernandez.guerra.control;
 
+import hernandez.guerra.exceptions.AccommodationProviderException;
+import hernandez.guerra.model.Accommodation;
+
 public interface AccommodationStore {
+    void save(Accommodation accommodation) throws AccommodationProviderException;
 }
--- accommodation-provider/src/main/java/hernandez/guerra/control/JMSAccommodationStore.java
+++ accommodation-provider/src/main/java/hernandez/guerra/control/JMSAccommodationStore.java
@@ -1,4 +1,54 @@
 package hernandez.guerra.control;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import hernandez.guerra.exceptions.AccommodationProviderException;
+import hernandez.guerra.model.Accommodation;
+import jakarta.jms.*;
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import java.time.Instant;
+
 public class JMSAccommodationStore implements AccommodationStore{
+    private final String topicName;
+    private final String url;
+
+    public JMSAccommodationStore(String topicName, String url) {
+        this.topicName = topicName;
+        this.url = url;
+    }
+
+    @Override
+    public void save(Accommodation accommodation) throws AccommodationProviderException {
+        try (Connection connection = createConnection()) {
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            Topic destination = session.createTopic(topicName);
+            MessageProducer producer = session.createProducer(destination);
+
+            TextMessage message = createTextMessage(accommodation, session);
+            producer.send(message);
+
+        } catch (JMSException e) {
+            throw new AccommodationProviderException(e.getMessage(), e);
+        }
+    }
+
+    private TextMessage createTextMessage(Accommodation accommodation, Session session) throws JMSException {
+        String jsonAccommodation = convertAccommodationToJson(accommodation);
+        return session.createTextMessage(jsonAccommodation);
+    }
+
+    private Connection createConnection() throws JMSException {
+        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(url);
+        Connection connection = connectionFactory.createConnection();
+        connection.start();
+        return connection;
+    }
+
+    private String convertAccommodationToJson(Accommodation accommodation) {
+        Gson gson = new GsonBuilder()
+                .registerTypeAdapter(Instant.class, new InstantTypeAdapter())
+                .create();
+        return gson.toJson(accommodation);
+    }
 }
